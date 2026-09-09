50b68f367090f6fe7eec7400bca90abb9f3cdb80
kintu hamuzah <kintuhamuzah66@gmail.com>
2026-09-09 20:50:13 +0300

    add render url to ALLOWED_HOSTS

--- mane/settings.py
+++ mane/settings.py
@@ -29,8 +29,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "mane-api.onrender.com",
+]
 
 # Application definition
 
